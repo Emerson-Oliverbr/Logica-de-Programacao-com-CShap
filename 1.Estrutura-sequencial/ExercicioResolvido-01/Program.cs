8add36765dfad534b2e82096015dc6bb73b94bce
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-22 21:57:00 -0300

    Aprendendo Saída de dados

--- 1.Estrutura-sequencial/ExercicioResolvido-01/Program.cs
+++ 1.Estrutura-sequencial/ExercicioResolvido-01/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            String nome = "Emerson";
+            int idade = 39;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(x.ToString("F4", CultureInfo.InvariantCulture) + "\n");                    
+            Console.WriteLine("Resultado é " + x);
+            Console.WriteLine("Resultado é " + x.ToString("F3",CultureInfo.InvariantCulture));
+            System.Console.WriteLine("O valor do troco é R$: " + x + " Reais");
+            System.Console.WriteLine($"Meu nome é {nome} eu tenho {idade} anos");
+        }
+    }
+}
