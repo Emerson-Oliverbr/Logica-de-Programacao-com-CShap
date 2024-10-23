f07326812594cb0f08e968b718cb4177ff79d6e4
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-22 22:11:19 -0300

    Aprendendo processamento de dados e casting

---
+++ 1.Estrutura-sequencial/2.Processamento de dados e casting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 5;
+            y = 2 * x;
+
+            System.Console.WriteLine("Valor de x é: " + x);
+            System.Console.WriteLine("Valor de y é: " + y);
+
+            double b, B, h, area;
+
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+            area = (b + B) / 2.0 * h;
+
+            System.Console.WriteLine("Area é = " + area);
+        }
+    }
+}
