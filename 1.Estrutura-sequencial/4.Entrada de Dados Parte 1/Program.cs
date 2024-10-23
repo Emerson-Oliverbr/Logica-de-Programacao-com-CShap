09a331107ac00749b0a4322f0c9bb53625e7853b
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-22 23:19:34 -0300

    Entrada de Dados Parte-1

---
+++ 1.Estrutura-sequencial/4.Entrada de Dados Parte 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+           
+
+            string[] vetFrutas = Console.ReadLine().Split(' ');
+
+            string fruta1 = vetFrutas[0];
+            string fruta2 = vetFrutas[1];
+            string fruta3 = vetFrutas[2];
+
+            System.Console.WriteLine("Voce digitou: ");
+            System.Console.WriteLine(frase);
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+            System.Console.WriteLine(z);
+            System.Console.WriteLine();
+            System.Console.WriteLine(fruta1);
+            System.Console.WriteLine(fruta2);
+            System.Console.WriteLine(fruta3);      
+
+        }
+    }
+}
