13276fc0cc7a49c79c3129e5531796e7c79b8ebe
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-22 23:59:37 -0300

    Entrada de dados Dados Parte-2

---
+++ 1.Estrutura-sequencial/4.Entrada de Dados Parte 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int numero1 = int.Parse(Console.ReadLine());
+            char letra = char.Parse(Console.ReadLine());
+            double numero2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            string[] dadosCadastrais = Console.ReadLine().Split(' ');
+            string nome = dadosCadastrais[0];
+            char sexo = char.Parse(dadosCadastrais[1]);
+            int idade = int.Parse(dadosCadastrais[2]);
+            double altura = double.Parse(dadosCadastrais[3],CultureInfo.InstalledUICulture);
+
+            Console.WriteLine("Voce digitou: ");
+            Console.WriteLine(numero1);
+            Console.WriteLine(letra);
+            Console.WriteLine(numero2.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);    
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));    
+        }
+    }
+}
