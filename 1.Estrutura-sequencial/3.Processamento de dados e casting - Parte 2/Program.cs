ad2ca561364f40fed7fa06adc9c7aafd56d29a83
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-22 22:26:17 -0300

    Aprendendo processamento de dados e casting Parte-2

---
+++ 1.Estrutura-sequencial/3.Processamento de dados e casting - Parte 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+            System.Console.WriteLine();
+            double x = 5.4;
+            int y = (int) x;
+
+            resultado = (double) a / b;
+
+            System.Console.WriteLine(resultado.ToString("F1",CultureInfo.InvariantCulture));
+            System.Console.WriteLine(y);
+        }
+    }
+}
