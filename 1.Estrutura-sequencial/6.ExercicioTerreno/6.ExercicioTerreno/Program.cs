10b6bc90ced68fb906e71675e38aa0e5c3ef2899
Emerson-Oliverbr <emerson.oliverbr@gmail.com>
2024-10-26 20:34:40 -0300

    Exercicio Terreno

---
+++ 1.Estrutura-sequencial/6.ExercicioTerreno/6.ExercicioTerreno/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioTerreno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = largura * comprimento;
+            double precoTotal = area * precoMetroQuadrado; 
+
+            Console.WriteLine("Area = " + area.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + precoTotal.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
