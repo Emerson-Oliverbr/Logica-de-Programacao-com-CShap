423c5e36bbf63b8bad21103e56eae4f8865ec715
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-29 21:30:09 -0300

    Add: Exercicios Resolvidos em Aula

---
+++ 1.Estrutura-sequencial/ExercicioResolvido-03/ExercicioResolvido-03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] dados = Console.ReadLine().Split(" ");
+            string nome1 = (dados[0]);
+            int idade1 = int.Parse(dados[1]);
+
+            dados = Console.ReadLine().Split(" ");
+            string nome2 = (dados[0]);
+            int idade2 = int.Parse(dados[1]);
+
+            double idadeMedia = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine($"A idade media de {nome1} e {nome2} é de {idadeMedia.ToString("F1",CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
