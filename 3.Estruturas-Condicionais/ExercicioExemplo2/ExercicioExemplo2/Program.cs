1bcc8845645a69e3beb973fc7ea5c53dc38d0f1e
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-05 20:10:14 -0300

    Iniciando Estruturas condicionais

---
+++ 3.Estruturas-Condicionais/ExercicioExemplo2/ExercicioExemplo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExercicioExemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int valor1 = int.Parse(valores[0]);
+            int valor2 = int.Parse(valores[1]);
+            int valor3 = int.Parse(valores[2]);
+
+            int menorValor;
+
+            if (valor1 < valor2 && valor1 < valor3)
+            {
+                menorValor = valor1;
+            }
+            else 
+                if (valor2 < valor1 && valor2 < valor3)
+            {
+                menorValor = valor2;
+            }
+            else
+            {
+                menorValor = valor3;
+            }
+
+            Console.WriteLine($"Menor valor é: {menorValor}");
+
+            Console.ReadKey();
+        }
+    }
+}
