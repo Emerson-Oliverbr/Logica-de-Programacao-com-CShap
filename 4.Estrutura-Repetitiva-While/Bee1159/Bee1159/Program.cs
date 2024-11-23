60ea79632d718734d06f7e76d839078639d3707c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-22 23:13:32 -0300

    Finalizando estrutura while

---
+++ 4.Estrutura-Repetitiva-While/Bee1159/Bee1159/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bee1159
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            int somaPares = 0;
+            while (x != 0)
+            {
+                if (x % 2!= 0)
+                {
+                    x = x + 1;
+                }
+
+                somaPares = x + x + 2 + x + 4 + x + 6 + x + 8;
+
+                Console.WriteLine(somaPares);
+                x = int.Parse(Console.ReadLine());                
+            }
+        }
+    }
+}
