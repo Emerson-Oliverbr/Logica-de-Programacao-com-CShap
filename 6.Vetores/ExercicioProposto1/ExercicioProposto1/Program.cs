893d409dfc210cccb763eb96c20074cbe034754a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-11 20:42:38 -0300

    Add: primeiro exercicio sobre vetores

---
+++ 6.Vetores/ExercicioProposto1/ExercicioProposto1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            double[] numeros = new double[N];
+            string[] numerosDigitados = Console.ReadLine().Split(' ');
+
+            for (int index=0; index<N; index+=1)
+            {                
+                numeros[index] = double.Parse(numerosDigitados[index],CultureInfo.InvariantCulture);
+            }
+
+            double maiorValor = numeros[0];
+            int posicaoMaior = 0;
+
+            for (int index = 1; index < N; index += 1)
+            {
+                if (numeros[index] > maiorValor)
+                {
+                    maiorValor = numeros[index];
+                    posicaoMaior = index;
+                }
+            }
+            Console.WriteLine(maiorValor.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicaoMaior);
+        }
+    }
+}
