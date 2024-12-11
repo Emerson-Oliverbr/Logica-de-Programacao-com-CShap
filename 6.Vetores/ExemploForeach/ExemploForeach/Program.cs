79ec102970f76ba9473a5b59ce9972f2cfe3c941
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-11 19:54:03 -0300

    Aprendendo sintaxe foreach

---
+++ 6.Vetores/ExemploForeach/ExemploForeach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExemploForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] vet = new string[N];
+
+            for(int index=0; index<N; index+=1)
+            {
+                vet[index] = Console.ReadLine();
+            }
+            Console.WriteLine($"Nomes lidos: ");
+            foreach (string elemento in vet)
+            {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}
